f4e9e075c80fba46047151571598f06b0223974a
juliano Telles <juliano.telles@gmail.com>
2025-07-18 16:07:22 -0300

    feat: Adiciona botao 'Iniciar quiz' com a seta na StartScreen

--- segundo_app/lib/start_screen.dart
+++ segundo_app/lib/start_screen.dart
@@ -19,12 +19,13 @@ class StartScreen extends StatelessWidget {
             style: TextStyle(color: Colors.white, fontSize: 24),
           ),
           const SizedBox(height: 30),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            child: const Text('Iniciar Quiz'),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Iniciar Quiz'),
           ),
         ],
       ),
