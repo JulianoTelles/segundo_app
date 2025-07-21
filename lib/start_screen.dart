796857caac204b9c31166ad8c8317a206516cdf9
juliano Telles <juliano.telles@gmail.com>
2025-07-21 16:36:25 -0300

    feat: Implementa logica de troca de tela e botao iniciar quiz

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -15,12 +17,12 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 80),
           const Text(
-            'Prof. Juliano Telles - 2025 ',
+            ' Prof. Juliano Telles - 2025',
             style: TextStyle(color: Colors.white, fontSize: 24),
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,            
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
